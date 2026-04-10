8c2684cd8d27d90dfbb6f7ee4f2e262d0d59ed42
yoyochen088 <somnolentcat@gmail.com>
2026-04-10 14:37:58 +0800

    Add files via upload

---
+++ flowers.js
@@ -0,0 +1,271 @@
+window.FLOWERS = [
+  {
+    "id": 1,
+    "name": "仙魅靈狐",
+    "img": "FlowerImage/仙/仙魅靈狐.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "花靈商店",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 2,
+    "name": "夜夢花庭",
+    "img": "FlowerImage/仙/夜夢花庭.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "活動",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 3,
+    "name": "夢幻之心",
+    "img": "FlowerImage/仙/夢幻之心.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "心聯盟",
+    "owners": []
+  },
+  {
+    "id": 4,
+    "name": "天上宮闕",
+    "img": "FlowerImage/仙/天上宮闕.jpg",
+    "quality": "仙",
+    "score": 30,
+    "obtain": "花靈商店",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 5,
+    "name": "星夜奇遇",
+    "img": "FlowerImage/仙/星夜奇遇.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "活動",
+    "owners": []
+  },
+  {
+    "id": 6,
+    "name": "星河映蕊",
+    "img": "FlowerImage/仙/星河映蕊.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "星河映蕊",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 7,
+    "name": "有龍則靈",
+    "img": "FlowerImage/仙/有龍則靈.jpg",
+    "quality": "仙",
+    "score": 30,
+    "obtain": "活動",
+    "owners": []
+  },
+  {
+    "id": 8,
+    "name": "杏月·花影雅憩",
+    "img": "FlowerImage/仙/杏月·花影雅憩.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "精靈花",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 9,
+    "name": "柳月·溪亭燕語",
+    "img": "FlowerImage/仙/柳月·溪亭燕語.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "精靈花",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 10,
+    "name": "桂月·秋千稚語",
+    "img": "FlowerImage/仙/桂月·秋千稚語.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "精靈花",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 11,
+    "name": "梅月·疏影聽弦",
+    "img": "FlowerImage/仙/梅月·疏影聽弦.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "精靈花",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 12,
+    "name": "歡雪頌冬",
+    "img": "FlowerImage/仙/歡雪頌冬.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "活動",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 13,
+    "name": "歡雲妙舞",
+    "img": "FlowerImage/仙/歡雲妙舞.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "卡冊",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 14,
+    "name": "焰火迎春",
+    "img": "FlowerImage/仙/焰火迎春.jpg",
+    "quality": "仙",
+    "score": 30,
+    "obtain": "活動",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 15,
+    "name": "碧羽青鸞",
+    "img": "FlowerImage/仙/碧羽青鸞.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "花靈商店",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 16,
+    "name": "福運招財",
+    "img": "FlowerImage/仙/福運招財.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "活動",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 17,
+    "name": "紫霄雲庭",
+    "img": "FlowerImage/仙/紫霄雲庭.jpg",
+    "quality": "仙",
+    "score": 30,
+    "obtain": "花靈商店",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 18,
+    "name": "繁花滿築",
+    "img": "FlowerImage/仙/繁花滿築.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "卡冊",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 19,
+    "name": "花宴流芳",
+    "img": "FlowerImage/仙/花宴流芳.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "活動",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 20,
+    "name": "花漾春山",
+    "img": "FlowerImage/仙/花漾春山.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "活動",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 21,
+    "name": "芳華尋蝶",
+    "img": "FlowerImage/仙/芳華尋蝶.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "卡冊",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 22,
+    "name": "菊月·花間飛鳶",
+    "img": "FlowerImage/仙/菊月·花間飛鳶.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "精靈花",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 23,
+    "name": "葭月·棲舟泛漪",
+    "img": "FlowerImage/仙/葭月·棲舟泛漪.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "精靈花",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 24,
+    "name": "金麟飛渡",
+    "img": "FlowerImage/仙/金麟飛渡.jpg",
+    "quality": "仙",
+    "score": 30,
+    "obtain": "活動",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  },
+  {
+    "id": 25,
+    "name": "陽月·芙香盈袖",
+    "img": "FlowerImage/仙/陽月·芙香盈袖.jpg",
+    "quality": "仙",
+    "score": 28,
+    "obtain": "精靈花",
+    "owners": [
+      "蜜桃香檳"
+    ]
+  }
+];
---
+++ members.js
@@ -0,0 +1,26 @@
+window.MEMBERS = [
+  {
+    "id": 1,
+    "gameId": "gt1ezkqjvnk",
+    "nickname": "小矓兒",
+    "note": "會長"
+  },
+  {
+    "id": 2,
+    "gameId": "gt11a3d84if",
+    "nickname": "儀兒",
+    "note": "副會長"
+  },
+  {
+    "id": 3,
+    "gameId": "gt1kkwtmea7",
+    "nickname": "Hoi",
+    "note": "副會長"
+  },
+  {
+    "id": 4,
+    "gameId": "gt021477vjj",
+    "nickname": "蜜桃香檳",
+    "note": "菁英"
+  }
+];
